762a9e8b5976a95b15acaed4b7b784fd36b1d0dc
silaswpo <silaswesleypereiradeoliveira@gmail.com>
2025-06-25 19:23:52 -0300

    Validando

--- memory.v
+++ memory.v
@@ -9,10 +9,36 @@ module Memory #(
 
     input  wire [31:0] addr_i,     // Endereço
     input  wire [31:0] data_i,     // Dados de entrada (para escrita)
-    output wire [31:0] data_o,     // Dados de saída (para leitura)
+    output reg [31:0] data_o,     // Dados de saída (para leitura)
 
-    output wire        ack_o       // Confirmação da transação
+    output reg        ack_o       // Confirmação da transação
 );
 
 
+    // Memória de 32 bits por posição (palavra)
+    reg [31:0] memory [0:MEMORY_SIZE-1];
+
+    initial begin
+        if (MEMORY_FILE != "") begin
+            $readmemh(MEMORY_FILE, memory);
+        end
+    end
+
+    wire [31:2] aligned_addr = addr_i[31:2];
+
+    always @(posedge clk) begin
+        ack_o <= rd_en_i || wr_en_i;  // Confirma qualquer operação válida
+
+        if (rd_en_i) begin
+            data_o <= memory[aligned_addr];
+        end else begin
+            data_o <= 32'd0;
+        end
+
+        if (wr_en_i) begin
+            memory[aligned_addr] <= data_i;
+        end
+    end
+
 endmodule
+
--- registers.v
+++ registers.v
@@ -11,5 +11,23 @@ module Registers (
     output wire [31:0] RS2_data_o
 );
 
+    // Banco de 32 registradores de 32 bits
+    reg [31:0] regs [0:31];
+
+    // Leitura assíncrona
+    assign RS1_data_o = regs[RS1_ADDR_i];
+    assign RS2_data_o = regs[RS2_ADDR_i];
+
+    // Escrita síncrona
+    always @(posedge clk) begin
+        if (wr_en_i && (RD_ADDR_i != 5'd0)) begin
+            regs[RD_ADDR_i] <= data_i;
+        end
+    end
+
+    // Garantir que o registrador x0 (posição 0) seja sempre 0
+    always @(posedge clk) begin
+        regs[0] <= 32'd0;
+    end
 
 endmodule
